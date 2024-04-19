7e8b9d2f7e4d8a01b508a140eea123cf9ec28198
yousrakdc <yousrakdc@gmail.com>
2024-04-19 09:30:57 +0000

    modified

--- very_simple_shell.c
+++ very_simple_shell.c
@@ -9,18 +9,39 @@
 
 int main(void)
 {
-        char *file;
+	char *file;
+	list_path *head = NULL;
+	char *value = NULL;
 
-        while (1)
-        {
-                file = get_filename();
-                if (!file)
-                        break;
-                /* builtins_check(file); */
-                execute_it(file);
-                free(file);
-        }
-        return (0);
+	value = print_env("PATH");
+
+	if (value)
+	{
+		head = _path(value);
+	}
+	free (value);
+
+	while (1)
+	{
+		file = get_filename();
+		if (!file)
+			break;
+
+		file = which_path(file, head);
+
+		if (file)
+		{
+			execute_it(file);
+			free(file);
+		}
+		else
+			execute_it(file);
+	}
+	free(file);
+
+	if (head)
+		free_list(head);
+	return (0);
 }
 
 /**
@@ -30,22 +51,22 @@ int main(void)
 
 char *get_filename()
 {
-        char *filename = NULL;
-        size_t length;
-        int input;
+	char *filename = NULL;
+	size_t length;
+	int input;
 
 
-        printf("~B ");
-        input = getline(&filename, &length, stdin);
+	printf("~B ");
+	input = getline(&filename, &length, stdin);
 
-        if (input == -1)
-        {
-                printf("ctrl D \n");
-                free(filename);
-                exit(EXIT_FAILURE); /* Better error handling */
-        }
+	if (input == -1)
+	{
+		printf("ctrl D \n");
+		free(filename);
+		exit(EXIT_FAILURE); /* Better error handling */
+	}
 
-        return (filename);
+	return (filename);
 
 }
 
@@ -58,42 +79,42 @@ char *get_filename()
 
 int execute_it(char *filename)
 {
-        pid_t pid;
-        char *argv[MAX_ARGS + 1]; /* One extra for NULL */
-        int argc = 0;
-        char *token;
+	pid_t pid;
+	char *argv[MAX_ARGS + 1]; /* One extra for NULL */
+	int argc = 0;
+	char *token;
 
-        token = strtok(filename, " ");
+	token = strtok(filename, " ");
 
-        while (token != NULL && argc < MAX_ARGS)
-        {
-                argv[argc++] = token;
-                token = strtok(NULL, " ");
-        }
+	while (token != NULL && argc < MAX_ARGS)
+	{
+		argv[argc++] = token;
+		token = strtok(NULL, " ");
+	}
 
-        argv[argc] = NULL;
+	argv[argc] = NULL;
 
-        pid = fork();
+	pid = fork();
 
-        if (pid == 0)
-        {
-                printf("execute command : %s \n", filename);
-                if (execv(argv[0], argv) == -1)
-                {
-                        perror("Error");
-                        exit(EXIT_FAILURE);
-                }
-        }
-        else if (pid == -1)
-        {
-                printf("Error : fork failure");
-                exit(EXIT_FAILURE);
-        }
-        else
-        {
-                printf("I'm waiting \n");
-                waitpid(pid, NULL, 0);
-        }
+	if (pid == 0)
+	{
+		printf("execute command : %s \n", filename);
+		if (execv(argv[0], argv) == -1)
+		{
+			perror("Error");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else if (pid == -1)
+	{
+		printf("Error : fork failure");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		printf("I'm waiting \n");
+		waitpid(pid, NULL, 0);
+	}
 
-        return (0);
+	return (0);
 }
