79a3b7c194d4534c84271f29944c79735efa4319
yousrakdc <yousrakdc@gmail.com>
2024-04-19 09:28:54 +0000

    modified

---
+++ print_env.c
@@ -0,0 +1,38 @@
+#include "shell.h"
+
+/**
+ * print_env - built-in to prints the environment
+ * @args: array of arguments
+ */
+
+char *print_env(const char *name)
+{
+	const char *value = getenv(name);
+	char *copy;
+
+	/* Check if the environment variable exists */
+	if (value != NULL)
+	{
+		/* Make a copy of the string */
+		copy = malloc(strlen(value) + 1);
+		if (copy != NULL)
+		{
+			strcpy(copy, value);
+			/* Print the name and value of the environment variable */
+			printf("%s = %s\n", name, copy);
+			return (copy);
+		}
+		else
+		{
+			/* Handle memory allocation failure */
+			printf("Error: Unable to allocate memory\n");
+			return (NULL);
+		}
+	}
+	else
+	{
+		/* If the environment variable is not found, print a message */
+		printf("%s = not found\n", name);
+		return (NULL);
+	}
+}
