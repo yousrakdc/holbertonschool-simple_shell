bb3b110a706b1490fdef730d3702a9fc9809da52
yousrakdc <yousrakdc@gmail.com>
2024-04-23 12:18:50 +0000

    bettied

--- shell.h
+++ shell.h
@@ -11,20 +11,9 @@
 #include <sys/wait.h>
 #include <errno.h>
 
-/* Externe variable */
+/* Extern variable */
 
 extern char **environ;
-
-void _puts(char *str);
-int _strlen(char *s);
-
-void _isatty(void);
-int main(void);
-char *get_command();
-char *get_filename();
-int execute_it(char *);
-
-/* path */
 
 /**
  * struct list_path - inked list of path directories
@@ -38,19 +27,33 @@ typedef struct list_path
 	struct list_path *next;
 } list_path;
 
+void _puts(char *str);
+int _strlen(char *s);
+
+/* main */
+
+int main(void);
+char *get_command();
+char **parse_command(char *command);
+int execute_it(char *command, list_path *head);
+/* path */
+
 list_path *_path(const char *path);
 list_path *add_node_end(list_path **head, const char *path);
 char *which_path(char *filename, list_path *head);
 void free_list(list_path *head);
 
-/* prototypes for builtins*/
-
-void print_env(char **environ);
-void exit_program(char *command, list_path *head, char *resolved_path);
-
 /* implementation of getenv*/
 
 char *_getenv(const char *name);
+
+/* prototype for print_env*/
+
+void print_env(char **environ);
+
+/* prototype for exit */
+
+void exit_program(char *command, list_path *head);
 
 /* token */
 
