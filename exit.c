7bb332db6b7cf5d65b5762b6a6e6183e2bb5c038
yousrakdc <yousrakdc@gmail.com>
2024-04-23 12:17:59 +0000

    bettied

--- exit.c
+++ exit.c
@@ -5,28 +5,25 @@ void free_list(list_path *head);
 /**
  * exit_program - built-in to exits the shell
  * @command: Pointer to the command string. Will be freed if not NULL.
- * @resolved_path: Pointer to the resolved path string. Freed if not NULL.
  * @head: Pointer to the head of the linked list of paths. Will be freed.
  */
 
-void exit_program(char *command, list_path *head, char *resolved_path)
+void exit_program(char *command, list_path *head)
 {
+	/* If the command string is not NULL */
 	if (command)
 	{
 		free(command);
-		command = NULL;
+		command = NULL; /* Set the command pointer to NULL for safety*/
 	}
 
-	if (resolved_path)
-	{
-		free(resolved_path);
-		resolved_path = NULL;
-	}
-
+	/* If the head of the path list is not NULL*/
 	if (head)
 	{
+		/*Free the linked list of paths using the free_list function*/
 		free_list(head);
 	}
 
+	/* Exit the program with status code 0 (success)*/
 	exit(0);
 }
