7c1b5c330025e61c79ff6617006b2a74acd014b6
yousrakdc <yousrakdc@gmail.com>
2024-04-23 12:15:42 +0000

    bettied

--- print_env.c
+++ print_env.c
@@ -2,6 +2,7 @@
 
 /**
  * print_env - built-in to prints the environments
+ * @environ: access to the environment variables
  */
 
 void print_env(char **environ)
@@ -9,9 +10,11 @@ void print_env(char **environ)
 	int i = 0;
 
 	printf("My environment:\n");
+
+	/*Loop through the array of environment variables.*/
 	while (environ[i] != NULL)
 	{
-		printf("%s\n", environ[i]);
-		i++;
+		printf("%s\n", environ[i]); /*Print each environment variable.*/
+		i++; /*Increment the counter to move to the next environment variable*/
 	}
 }
