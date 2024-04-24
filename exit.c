b1c05c0b52624072016ba887cbbccc8c8e3d0555
SamJouini <samantha.u.h.jouini@gmail.com>
2024-04-24 11:23:31 +0200

    betty and annotations

--- exit.c
+++ exit.c
@@ -8,16 +8,20 @@
 
 void exit_program(char *command, list_path *head)
 {
+	/* If the command string is not NULL */
 	if (command)
 	{
 		free(command);
-		command = NULL;
+		command = NULL; /* Set the command pointer to NULL for safety */
 	}
 
+	/* If the head of the path list is not NULL */
 	if (head)
 	{
+		/* Free the linked list of paths using the free_list function */
 		free_list(head);
 	}
 
+	/* Exit the program with status code 0 (success) */
 	exit(0);
 }
