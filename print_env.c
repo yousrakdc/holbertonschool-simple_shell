fda0273e5eb7832ebc87e6fad328094d3add2470
SamJouini <samantha.u.h.jouini@gmail.com>
2024-04-24 10:57:31 +0200

    betty and annotations

--- print_env.c
+++ print_env.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * print_env - built-in to prints the environments
+ * print_env - built-in to prints the environment
  * @environ: environment of the program
  */
 
@@ -10,9 +10,11 @@ void print_env(char **environ)
 	int i = 0;
 
 	printf("My environment:\n");
+
+	/*Loop through the array of environment variables.*/
 	while (environ[i] != NULL)
 	{
-		printf("%s\n", environ[i]);
-		i++;
+		printf("%s\n", environ[i]); /*Print each environment variable.*/
+		i++; /*Increment the counter to move to the next variable*/
 	}
 }
